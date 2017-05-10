c306b61994ffcd79875a6c7e8f18b0d789427f83
JM1234 <jvestopa@up.edu.ph>
2017-05-10 10:01:25 +0800

    Part 2 added. Not yet done.

--- projects/proj3_measurement/traceroute.py
+++ projects/proj3_measurement/traceroute.py
@@ -1,19 +1,36 @@
 import subprocess
 import socket
+import time
+import re
 
 class TraceRT:
+	
 	def run_traceroute(self, hostnames, num_packets, output_filename):
-	
-		for hostname in hostnames:			
-			traceroute = subprocess.Popen(["traceroute", '-A', '-q', num_packets, hostname], stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
-	
-		#socket.gethostbyaddr("IP")	#reverse dns lookup
+		
+		timestamp = int(time.time())
+		self.host = {}
+
+		for hostname in hostnames:	
+			traceroute = subprocess.Popen(["traceroute", '-I', '-A', '-q', num_packets, hostname], stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
+		
+		hop={}
 		for line in iter(traceroute.stdout.readline, ""):
-			print(line)
+					
+			if not line.startswith('traceroute'):
+				print line
+				m= line.split()
+				print m[1]
+				name= self.gethostname(m[1])
+				print name				
+
+	def gethostname(self, ip):
+		if socket.gethostname().find('.')>=0:
+			return socket.gethostname()
+		else:
+			return socket.gethostbyaddr(socket.gethostname())[0]
 
 	def parse_traceroute(raw_traceroute_filename, output_filename) :
 		pass
 
-
 a= TraceRT()
-a.run_traceroute(['google.com'], "5", "traceroute.json")
+a.run_traceroute(['upd.edu.ph'], "5", "traceroute.json")
