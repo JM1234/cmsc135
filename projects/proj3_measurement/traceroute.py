7a4de9886c3a80123fb659106d6403fa59eebd1a
JM1234 <jvestopa@up.edu.ph>
2017-05-13 18:26:35 +0800

    Added traceroute parsing.

--- projects/proj3_measurement/traceroute.py
+++ projects/proj3_measurement/traceroute.py
@@ -2,6 +2,8 @@ import subprocess
 import socket
 import time
 import re
+import json
+import os
 
 class TraceRT:
 	
@@ -9,28 +11,59 @@ class TraceRT:
 		
 		timestamp = int(time.time())
 		self.host = {}
+		self.hop = []
 
+		self.host['timestamp'] = timestamp
 		for hostname in hostnames:	
-			traceroute = subprocess.Popen(["traceroute", '-I', '-A', '-q', num_packets, hostname], stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
+			self.host[hostname] = []
+			traceroute = subprocess.Popen(["traceroute", '-A', '-q', num_packets, hostname], stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
 		
-		hop={}
-		for line in iter(traceroute.stdout.readline, ""):
+			for line in iter(traceroute.stdout.readline, ""):
 					
-			if not line.startswith('traceroute'):
-				print line
-				m= line.split()
-				print m[1]
-				name= self.gethostname(m[1])
-				print name				
+				if not line.startswith('traceroute'):
+									
+					print line
+					m= line.split()
+					asn = 'None'
+					ip = m[1]
+					name = m[2].strip('()')
+					if (m[3] != "[*]"):			
+						asn = m[3].strip('[]')	
 
+					self.addHop(hostname, name, ip, asn)
+
+			result = [dict(tupleized) for tupleized in set(tuple(item.items()) for item in self.hop)]
+			self.host[hostname].append(result)		
+		self.append_file(output_filename, self.host)
+
+	def addHop(self, host, name, ip, asn):
+		
+		self.hop.append( { \
+			'name' : name, \
+			'ip' : ip, \
+			'ASN' : asn \
+			} )
+
+	def append_file(self, filename, data):
+		with open(filename, 'a') as f:
+			json.dump(data, f)
+			f.write(os.linesep)
+
+	#def write_json(self, file_name, data):
+		#with open(file_name, 'w') as f:
+		#	json.dump(data, f)	
+			
+	def parse_traceroute(raw_traceroute_filename, output_filename) :
+		pass
+
+
+"""
 	def gethostname(self, ip):
 		if socket.gethostname().find('.')>=0:
 			return socket.gethostname()
 		else:
 			return socket.gethostbyaddr(socket.gethostname())[0]
-
-	def parse_traceroute(raw_traceroute_filename, output_filename) :
-		pass
+"""
 
 a= TraceRT()
-a.run_traceroute(['upd.edu.ph'], "5", "traceroute.json")
+a.run_traceroute(['twitter.com'], "5", "tr_a.json")
